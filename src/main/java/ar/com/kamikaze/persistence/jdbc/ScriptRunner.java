f877207b693dbe0853fbc9821fa7642f6e866b53
emystein <emystein@gmail.com>
2018-02-12 13:03:38 -0300

    Removed unnecessary ScriptExecutor and moved code to ScriptRunner

--- src/main/java/ar/com/kamikaze/persistence/jdbc/ScriptRunner.java
+++ src/main/java/ar/com/kamikaze/persistence/jdbc/ScriptRunner.java
@@ -1,5 +1,7 @@
 package ar.com.kamikaze.persistence.jdbc;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
 import java.io.Reader;
@@ -17,18 +19,55 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class ScriptRunner {
-	private final ScriptExecutor scriptExecutor;
+	protected ConnectionWrapper connection;
+	private boolean autoCommit;
+	boolean originalAutoCommit;
+	private boolean stopOnError;
+	private final ScriptParser scriptParser = new ScriptParser();
 
 	public ScriptRunner(Connection connection, boolean autoCommit, boolean stopOnError) throws SQLException {
-		this.scriptExecutor = new ScriptExecutor(connection, autoCommit, stopOnError);
+		this.connection = autoCommit ? new AutoCommitConnection(connection) : new ManualCommitConnection(connection);
+		this.autoCommit = autoCommit;
+		this.originalAutoCommit = connection.getAutoCommit();
+		this.connection.setAutoCommit(autoCommit);
+		this.stopOnError = stopOnError;
 	}
 
 	public void setDelimiter(String delimiter, boolean fullLineDelimiter) {
-		scriptExecutor.setDelimiter(delimiter, fullLineDelimiter);
+		scriptParser.setDelimiter(delimiter, fullLineDelimiter);
 	}
 
 	public void runScript(String scriptPath) throws IOException, SQLException {
-		scriptExecutor.runScript(scriptPath);
+		runScript(new BufferedReader(new FileReader(scriptPath)));
 	}
 
+	public void runScript(Reader reader) throws IOException, SQLException {
+		try {
+			connection.setAutoCommit(autoCommit);
+			List<ScriptCommand> commands = scriptParser.parse(reader);
+			run(commands);
+		} finally {
+			connection.setAutoCommit(originalAutoCommit);
+		}
+	}
+
+	private void run(List<ScriptCommand> commands) throws SQLException {
+		ResultSetPrinter resultSetPrinter = new ResultSetPrinter();
+
+		for (ScriptCommand command : commands) {
+			try {
+				ResultSet resultSet = connection.execute(command);
+				resultSetPrinter.print(resultSet);
+			} catch (SQLException e) {
+				log.error(e.getMessage(), e);
+
+				if (stopOnError) {
+					connection.rollback();
+					throw e;
+				}
+			}
+		}
+
+		connection.commit();
+	}
 }

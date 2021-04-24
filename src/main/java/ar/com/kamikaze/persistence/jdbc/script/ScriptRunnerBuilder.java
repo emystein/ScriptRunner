2bb855f5739e597838f51ebf37742a4d86ffa43a
Emiliano Menéndez <emystein@gmail.com>
2021-04-24 20:59:27 -0300

    Delete CommandRunnerBuilder.

--- src/main/java/ar/com/kamikaze/persistence/jdbc/script/ScriptRunnerBuilder.java
+++ src/main/java/ar/com/kamikaze/persistence/jdbc/script/ScriptRunnerBuilder.java
@@ -1,32 +1,45 @@
 package ar.com.kamikaze.persistence.jdbc.script;
 
-import ar.com.kamikaze.persistence.jdbc.commands.CommandRunnerBuilder;
+import ar.com.kamikaze.persistence.jdbc.commands.CommandRunner;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import static ar.com.kamikaze.persistence.jdbc.commands.CommandRunnerFactory.createAutoCommitCommandRunner;
+import static ar.com.kamikaze.persistence.jdbc.commands.CommandRunnerFactory.createManualCommitCommandRunner;
+
 public class ScriptRunnerBuilder {
-	private CommandRunnerBuilder commandRunnerBuilder;
+	private final Connection connection;
+	private boolean autoCommit;
+	private boolean stopOnError;
 
 	public static ScriptRunnerBuilder forConnection(Connection connection) {
 		return new ScriptRunnerBuilder(connection);
 	}
 
 	private ScriptRunnerBuilder(Connection connection) {
-		this.commandRunnerBuilder = CommandRunnerBuilder.wrap(connection);
+		this.connection = connection;
 	}
 
 	public ScriptRunnerBuilder autoCommit() {
-		this.commandRunnerBuilder.autoCommit();
+		this.autoCommit = true;
 		return this;
 	}
 
 	public ScriptRunnerBuilder stopOnError() {
-		this.commandRunnerBuilder.stopOnError();
+		this.stopOnError = true;
 		return this;
 	}
 
 	public ScriptRunner build() throws SQLException {
-		return new ScriptRunner(commandRunnerBuilder.build());
+		return new ScriptRunner(commandRunner());
+	}
+
+	private CommandRunner commandRunner() throws SQLException {
+		CommandRunner commandRunner = autoCommit ?
+				createAutoCommitCommandRunner(connection, stopOnError) :
+				createManualCommitCommandRunner(connection, stopOnError);
+
+		return commandRunner;
 	}
 }
